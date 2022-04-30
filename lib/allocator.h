d17abca219c569422c8ea02bb8229d0d468cad43
lvntky <klevent1903@gmail.com>
2022-05-01 02:02:07 +0300

    basic structure

---
+++ lib/allocator.h
@@ -0,0 +1,20 @@
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#include <stdio.h>
+
+typedef char ALIGN[16];
+union header
+{
+	struct
+	{
+		size_t size;
+		unsigned is_free;
+		union header* next;
+	}s;
+
+	ALIGN stub;
+};
+
+
+#endif
